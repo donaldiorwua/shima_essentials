4be2ae10222ef9d8d1adf24918b9f40c6e8e12ac
donaldiorwua <donaldiorwua@gmail.com>
2026-09-18 08:13:14 +0100

    Added admin privileges logic

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import (
+    Category, 
+    Product, 
+    Customer, 
+    DeliverySetting,
+    Order,
+    OrderItem
+)
 
 admin.site.register(Category)
 
@@ -7,5 +14,28 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "price", "available")
     search_fields = ("name", "description")
     list_filter = ("category", "available")
-    
+
 admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Customer)
+admin.site.register(DeliverySetting)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "order_number",
+        "customer_name",
+        "address",
+        "total",
+        "status",
+        "created_at",
+    )
+    inlines = [OrderItemInline]
